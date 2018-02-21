d6fc172fd9a568910c190a5f0bdf87e5013167dd
Ezra Nugroho <ezrahn@gmail.com>
2018-02-21 11:01:36 -0800

    Add an interface to play game in command line

---
+++ play_game.py
@@ -0,0 +1,15 @@
+from game import Game
+from funcs import playMatchesBetweenVersions
+import loggers as lg
+
+run_version = 1
+
+#use -1 for human player
+player1_version = -1
+player2_version = 25
+
+#how many rounds
+episodes = 2
+
+env = Game()
+playMatchesBetweenVersions(env, run_version, player1_version, player2_version, episodes, lg.logger_tourney, 0)
